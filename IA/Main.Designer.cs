5a9181f1775979319fceb22d05c6cb1dbb4ce2a9
jovace <jorge.vazquez.acevedo@alumnos.upm.es>
2018-12-06 21:13:09 +0100

    Algoritmo funcionando modo feo

--- IA/Main.Designer.cs
+++ IA/Main.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.mainTabControl = new System.Windows.Forms.TabControl();
             this.tabConfiguracion = new System.Windows.Forms.TabPage();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDibujar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.txtRutaDistancias = new System.Windows.Forms.TextBox();
             this.btnAbrirDistancias = new System.Windows.Forms.Button();
             this.lblDistancias = new System.Windows.Forms.Label();
@@ -37,10 +40,16 @@
             this.btnAbrirGrafo = new System.Windows.Forms.Button();
             this.lblGrafo = new System.Windows.Forms.Label();
             this.tabSimple = new System.Windows.Forms.TabPage();
+            this.txtTiempo = new System.Windows.Forms.TextBox();
+            this.lblTiempoRuta = new System.Windows.Forms.Label();
+            this.btnCalcularRuta = new System.Windows.Forms.Button();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.cbEstacionFinal = new System.Windows.Forms.ComboBox();
+            this.cbEstacionInicio = new System.Windows.Forms.ComboBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.openFileGrafo = new System.Windows.Forms.OpenFileDialog();
             this.openFileDistancias = new System.Windows.Forms.OpenFileDialog();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.mainTabControl.SuspendLayout();
             this.tabConfiguracion.SuspendLayout();
             this.tabSimple.SuspendLayout();
@@ -53,11 +62,13 @@
             this.mainTabControl.Location = new System.Drawing.Point(12, 12);
             this.mainTabControl.Name = "mainTabControl";
             this.mainTabControl.SelectedIndex = 0;
-            this.mainTabControl.Size = new System.Drawing.Size(908, 594);
+            this.mainTabControl.Size = new System.Drawing.Size(984, 705);
             this.mainTabControl.TabIndex = 0;
             // 
             // tabConfiguracion
             // 
+            this.tabConfiguracion.Controls.Add(this.btnStart);
+            this.tabConfiguracion.Controls.Add(this.btnDibujar);
             this.tabConfiguracion.Controls.Add(this.panel2);
             this.tabConfiguracion.Controls.Add(this.txtRutaDistancias);
             this.tabConfiguracion.Controls.Add(this.btnAbrirDistancias);
@@ -68,10 +79,39 @@
             this.tabConfiguracion.Location = new System.Drawing.Point(4, 22);
             this.tabConfiguracion.Name = "tabConfiguracion";
             this.tabConfiguracion.Padding = new System.Windows.Forms.Padding(3);
-            this.tabConfiguracion.Size = new System.Drawing.Size(900, 568);
+            this.tabConfiguracion.Size = new System.Drawing.Size(976, 679);
             this.tabConfiguracion.TabIndex = 0;
             this.tabConfiguracion.Text = "Configuracion";
             this.tabConfiguracion.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(482, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 48);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Empezar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnDibujar
+            // 
+            this.btnDibujar.Location = new System.Drawing.Point(389, 20);
+            this.btnDibujar.Name = "btnDibujar";
+            this.btnDibujar.Size = new System.Drawing.Size(86, 48);
+            this.btnDibujar.TabIndex = 7;
+            this.btnDibujar.Text = "Dibujar";
+            this.btnDibujar.UseVisualStyleBackColor = true;
+            this.btnDibujar.Click += new System.EventHandler(this.btnDibujar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(9, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(961, 599);
+            this.panel2.TabIndex = 6;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
             // 
             // txtRutaDistancias
             // 
@@ -127,21 +167,92 @@
             // 
             // tabSimple
             // 
+            this.tabSimple.Controls.Add(this.txtTiempo);
+            this.tabSimple.Controls.Add(this.lblTiempoRuta);
+            this.tabSimple.Controls.Add(this.btnCalcularRuta);
+            this.tabSimple.Controls.Add(this.lblFinal);
+            this.tabSimple.Controls.Add(this.lblInicio);
+            this.tabSimple.Controls.Add(this.cbEstacionFinal);
+            this.tabSimple.Controls.Add(this.cbEstacionInicio);
             this.tabSimple.Controls.Add(this.panel1);
             this.tabSimple.Location = new System.Drawing.Point(4, 22);
             this.tabSimple.Name = "tabSimple";
             this.tabSimple.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSimple.Size = new System.Drawing.Size(900, 568);
+            this.tabSimple.Size = new System.Drawing.Size(976, 679);
             this.tabSimple.TabIndex = 1;
             this.tabSimple.Text = "Vista simple";
             this.tabSimple.UseVisualStyleBackColor = true;
             // 
+            // txtTiempo
+            // 
+            this.txtTiempo.Location = new System.Drawing.Point(545, 13);
+            this.txtTiempo.Name = "txtTiempo";
+            this.txtTiempo.Size = new System.Drawing.Size(88, 20);
+            this.txtTiempo.TabIndex = 14;
+            // 
+            // lblTiempoRuta
+            // 
+            this.lblTiempoRuta.AutoSize = true;
+            this.lblTiempoRuta.Location = new System.Drawing.Point(438, 15);
+            this.lblTiempoRuta.Name = "lblTiempoRuta";
+            this.lblTiempoRuta.Size = new System.Drawing.Size(101, 13);
+            this.lblTiempoRuta.TabIndex = 13;
+            this.lblTiempoRuta.Text = "Tiempo de trayecto:";
+            // 
+            // btnCalcularRuta
+            // 
+            this.btnCalcularRuta.Location = new System.Drawing.Point(342, 11);
+            this.btnCalcularRuta.Name = "btnCalcularRuta";
+            this.btnCalcularRuta.Size = new System.Drawing.Size(89, 23);
+            this.btnCalcularRuta.TabIndex = 12;
+            this.btnCalcularRuta.Text = "Calcular Ruta";
+            this.btnCalcularRuta.UseVisualStyleBackColor = true;
+            this.btnCalcularRuta.Click += new System.EventHandler(this.btnCalcularRuta_Click);
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(657, 16);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(70, 13);
+            this.lblFinal.TabIndex = 11;
+            this.lblFinal.Text = "Estacion final";
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Location = new System.Drawing.Point(6, 16);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(75, 13);
+            this.lblInicio.TabIndex = 10;
+            this.lblInicio.Text = "Estacion inicio";
+            // 
+            // cbEstacionFinal
+            // 
+            this.cbEstacionFinal.FormattingEnabled = true;
+            this.cbEstacionFinal.Location = new System.Drawing.Point(738, 13);
+            this.cbEstacionFinal.Name = "cbEstacionFinal";
+            this.cbEstacionFinal.Size = new System.Drawing.Size(231, 21);
+            this.cbEstacionFinal.TabIndex = 9;
+            // 
+            // cbEstacionInicio
+            // 
+            this.cbEstacionInicio.FormattingEnabled = true;
+            this.cbEstacionInicio.Location = new System.Drawing.Point(87, 13);
+            this.cbEstacionInicio.Name = "cbEstacionInicio";
+            this.cbEstacionInicio.Size = new System.Drawing.Size(231, 21);
+            this.cbEstacionInicio.TabIndex = 8;
+            this.cbEstacionInicio.SelectedIndexChanged += new System.EventHandler(this.cbEstacionInicio_SelectedIndexChanged);
+            // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(7, 35);
+            this.panel1.Location = new System.Drawing.Point(8, 40);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(887, 527);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(961, 599);
+            this.panel1.TabIndex = 7;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
             // openFileGrafo
             // 
@@ -155,25 +266,18 @@
             this.openFileDistancias.InitialDirectory = "./";
             this.openFileDistancias.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDistancias_FileOk);
             // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(9, 74);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(885, 488);
-            this.panel2.TabIndex = 6;
-            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 618);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.mainTabControl);
             this.Name = "Main";
-            this.Text = "Main";
+            this.Text = "IA - Algoritmo de búsqueda A*";
             this.mainTabControl.ResumeLayout(false);
             this.tabConfiguracion.ResumeLayout(false);
             this.tabConfiguracion.PerformLayout();
             this.tabSimple.ResumeLayout(false);
+            this.tabSimple.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -191,8 +295,17 @@
         private System.Windows.Forms.Button btnAbrirDistancias;
         private System.Windows.Forms.Label lblDistancias;
         private System.Windows.Forms.OpenFileDialog openFileDistancias;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDibujar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.ComboBox cbEstacionFinal;
+        private System.Windows.Forms.ComboBox cbEstacionInicio;
+        private System.Windows.Forms.Button btnCalcularRuta;
+        private System.Windows.Forms.TextBox txtTiempo;
+        private System.Windows.Forms.Label lblTiempoRuta;
     }
 }
 
--- IA/Main.cs
+++ IA/Main.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.IO;
 using System.Linq;
@@ -13,14 +14,24 @@ namespace IA
 {
     public partial class Main : Form
     {
+        //Velocidad media del metro en metros/hora
+        const double VELMEDIA = 46000;
+
         Dictionary<string, Estacion> estaciones;
         Dictionary<int, string> estacionesName;
+        //double minlat=90, maxlat=0, minlng=180, maxlng=0;
+        bool inicioSet = false;
+        bool finalSet = false;
+        Estacion estacionInicio;
+        Estacion estacionFinal;
 
         public Main()
         {
             InitializeComponent();
             this.estaciones = new Dictionary<string, Estacion>();
             this.estacionesName = new Dictionary<int, string>();
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
         }
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
@@ -36,7 +47,6 @@ namespace IA
             for (int i = 0; i < filelines2.Length; i++) {
                 string[] data = filelines2[i].Split(';');
                 string nombre = data[0];
-                if (nombre.Equals("Shakhriston")) continue;
                 Estacion estacion = estaciones[nombre];
                 string[] vecinos = data[1].Trim('[').Trim(']').Split(',');
                 for (int j = 0; j < vecinos.Length; j++) {
@@ -65,6 +75,68 @@ namespace IA
             analizarFicheroDistancias();
         }
 
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Estacion item in estaciones.Values)
+            {
+                item.dibujar(e.Graphics);
+            }
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Estacion clicked=null;
+            bool encontrado = false;
+            for (int i = 0; i < estaciones.Values.Count && !encontrado; i++)
+            {
+                if (estaciones[estacionesName[i]].esDentro(e.X, e.Y)) {
+                    encontrado = true;
+                    clicked = estaciones[estacionesName[i]];
+                }
+            }
+            if (clicked == null)
+            {
+                return;
+            }
+
+            if (!inicioSet) {
+                estacionInicio = clicked;
+                cbEstacionInicio.Text = clicked.getName();
+                inicioSet = true;
+            }
+            else
+            {
+                if (!finalSet)
+                {
+                    estacionFinal = clicked;
+                    cbEstacionFinal.Text = clicked.getName();
+                    finalSet = true;
+                }
+                else
+                {
+                    finalSet = false;
+                    estacionInicio = clicked;
+                    cbEstacionInicio.Text = clicked.getName();
+                    inicioSet = true;
+                }
+            }
+        }
+
         private void analizarFicheroDistancias()
         {
             string[] filelines2 = File.ReadAllLines(openFileDistancias.FileName);
@@ -75,6 +147,10 @@ namespace IA
                 string nombre = linea[0];
                 double lat = Double.Parse(linea[1], CultureInfo.InvariantCulture);
                 double lng = Double.Parse(linea[2], CultureInfo.InvariantCulture);
+                /*if (lat < minlat) minlat = lat;
+                if (lat > maxlat) maxlat = lat;
+                if (lng < minlng) minlng = lng;
+                if (lng > maxlng) maxlng = lng;*/
                 Estacion estacion = new Estacion(nombre, lat, lng);
                 estaciones[nombre] = estacion;
                 estacionesName[i] = nombre;
@@ -95,14 +171,137 @@ namespace IA
 
             Console.WriteLine("Todo correcto");    
         }
+
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            foreach (String nombre in estaciones.Keys) {
+                cbEstacionFinal.Items.Add(nombre);
+                cbEstacionInicio.Items.Add(nombre);
+            }
+            panel2.Refresh();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Estacion item in estaciones.Values)
+            {
+                item.dibujar(e.Graphics);
+            }
+        }
+
+        private void cbEstacionInicio_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcularRuta_Click(object sender, EventArgs e)
+        {
+            if (!inicioSet || estacionInicio==null) {
+                //Error
+            }
+            if (!finalSet || estacionFinal==null) {
+                //Error
+            }
+
+            calcularRuta(estacionInicio, estacionFinal);
+        }
+
+        private void calcularRuta(Estacion estacionInicio, Estacion estacionFinal) {
+            HashSet<Estacion> conjuntoCerrado = new HashSet<Estacion>();
+            HashSet<Estacion> conjuntoAbierto = new HashSet<Estacion>();
+            Dictionary<Estacion, Estacion> vieneDe = new Dictionary<Estacion, Estacion>();
+            Dictionary<Estacion, double> gScore = new Dictionary<Estacion, double>();
+            Dictionary<Estacion, double> fScore = new Dictionary<Estacion, double>();
+
+            foreach (Estacion item in estaciones.Values) {
+                gScore[item] = double.PositiveInfinity;
+                fScore[item] = double.PositiveInfinity;
+            }
+
+            gScore[estacionInicio] = 0;
+            fScore[estacionInicio] = estacionInicio.getDist(estacionFinal.getName());
+
+            conjuntoAbierto.Add(estacionInicio);
+
+            while (conjuntoAbierto.Count != 0) {
+                Estacion actual = menorFScore(fScore, conjuntoAbierto);
+                if (actual.Equals(estacionFinal)) {
+                    txtTiempo.Text = Math.Ceiling((gScore[estacionFinal] / VELMEDIA) * 60).ToString();
+                    MessageBox.Show(reconstruirRuta(vieneDe, actual,estacionInicio), "Ruta más corta", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                conjuntoAbierto.Remove(actual);
+                conjuntoCerrado.Add(actual);
+
+                foreach (Estacion vecino in actual.getAdyacentes().Values) {
+                    if (conjuntoCerrado.Contains(vecino)) continue;
+
+                    double distAproximadaG = gScore[actual] + actual.getDist(vecino.getName());
+
+                    if (!conjuntoAbierto.Contains(vecino))
+                    {
+                        conjuntoAbierto.Add(vecino);
+                    }
+                    else if (distAproximadaG >= gScore[vecino]) {
+                        continue;
+                    }
+
+                    vieneDe[vecino] = actual;
+                    gScore[vecino] = distAproximadaG;
+                    fScore[vecino] = gScore[vecino] + vecino.getDist(estacionFinal.getName());
+                }
+
+            }
+
+
+        }
+
+        private String reconstruirRuta(Dictionary<Estacion, Estacion> vieneDe, Estacion actual, Estacion inicio) {
+            String ruta = "Ruta desde "+inicio.getName()+" hasta "+actual.getName()+": \n " + inicio.getName();
+            String listaEstaciones = "";
+            while (!actual.Equals(inicio)) {
+                listaEstaciones = " -> " + actual.getName() + listaEstaciones;
+                actual = vieneDe[actual];
+            }
+            return ruta + listaEstaciones;
+        }
+
+        private Estacion menorFScore(Dictionary<Estacion, double> fScore, HashSet<Estacion> conjuntoAbierto) {
+            Estacion menor = null;
+            double menorValor = double.PositiveInfinity;
+            foreach (Estacion item in conjuntoAbierto) {
+                if (fScore[item] < menorValor) {
+                    menorValor = fScore[item];
+                    menor = item;
+                }
+            }
+
+            return menor;
+        }
     }
 
     class Estacion {
+        const double MAXLAT = 41.352894;
+        const double MINLAT = 41.257376;
+        const double MAXLNG = 69.327885;
+        const double MINLNG = 69.195797;
+        const int ALTO = 555;
+        const int ANCHO = 917;
+        const int RADIO = 30;
+        
+
+
         string name;
         double lat;
         double lng;
         Dictionary<String, double> distances;
         Dictionary<String, Estacion> adyacentes;
+
+        Pen circulo;
+        Pen lin;
+        GraphicsPath gp;
+        Font letra;
+        Rectangle rec;
 
         public Estacion(string name, double lat, double lng) {
             this.name = name;
@@ -110,6 +309,11 @@ namespace IA
             this.lng = lng;
             this.distances = new Dictionary<string, double>();
             this.adyacentes = new Dictionary<string, Estacion>();
+            circulo = new Pen(Brushes.Black, 3);
+            gp = new GraphicsPath();
+            letra = new Font("arial", 10);
+            rec = new Rectangle(getPosX() - RADIO, getPosY() - RADIO, 2 * RADIO, 2 * RADIO);
+            lin = new Pen(Brushes.DimGray, 1);
         }
 
         public void putDist(string to, double dist) {
@@ -126,6 +330,73 @@ namespace IA
 
         public void conectar(Estacion vecino) {
             adyacentes[vecino.getName()] = vecino;
+        }
+
+        public int getPosY() {
+            double latDiff = MAXLAT - MINLAT;
+
+            int pix = (int)(((this.lat - MINLAT) / latDiff) * ALTO);
+
+            return 599-(pix+22);
+        }
+
+        public int getPosX()
+        {
+            double lngDiff = MAXLNG - MINLNG;
+
+            int pix = (int)(((this.lng - MINLNG) / lngDiff) * ANCHO);
+
+            return pix+22;
+        }
+
+        public bool esDentro(int clickX, int clickY) {
+            int difX=Math.Abs(this.getPosX()-clickX);
+            int difY = Math.Abs(this.getPosY() - clickY);
+            difX *= difX;
+            difY *= difY;
+            difX += difY;
+            if (Math.Sqrt(difX) < 21) { return true; } else { return false; }
+        }
+
+        public void dibujar(Graphics g) {
+            DibujaNodo(g);
+            DibujaArista(g);
+        }
+
+        public virtual void DibujaNodo(Graphics g)
+        {
+            g.FillPath(Brushes.Goldenrod, gp);
+            g.DrawPath(circulo, gp);
+            if (!this.name.Equals("Alisher-Navoi") && !this.name.Equals("Amir-Temur-Hiyoboni") && !this.name.Equals("Ming-Urik"))
+            {
+                g.DrawString(this.name, letra, Brushes.Black, rec, new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+            }
+        }
+
+        
+        public virtual void DibujaArista(Graphics g)
+        {
+            foreach (Estacion item in adyacentes.Values)
+            {
+                double tg = (double)(getPosY() - item.getPosY()) / (item.getPosX() - getPosX());
+                double atg = Math.Atan(tg);
+
+                int a = (int)(RADIO * Math.Cos(atg));
+                int b = (int)(RADIO * Math.Sin(atg));
+
+                if ((getPosX() < item.getPosX()))
+                {
+                    a *= -1;
+                    b *= -1;
+                }
+
+                Point p = new Point(item.getPosX() + a, item.getPosY() - b);
+                g.DrawLine(lin, getPosX(),getPosY(),item.getPosX(),item.getPosY());
+            }
+        }
+
+        public Dictionary<String, Estacion> getAdyacentes() {
+            return this.adyacentes;
         }
     }
 }
